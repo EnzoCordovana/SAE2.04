4c8764db8cb0d253e049c26d88d32b17b97eed14
EnzoCordovana <enzo.cordovana@etu.univ-amu.fr>
2025-03-24 09:50:27 +0100

    fix: creation des tables & ajout des not null

--- table.sql
+++ table.sql
@@ -9,36 +9,35 @@ PROMPT "Création des relations";
 CREATE TABLE PROPRIO(
     -- Attributes
     IdP NUMBER(6,0),
-    Nom VARCHAR2(20),
-    Prenom VARCHAR2(15),
+    Nom VARCHAR2(20) NOT NULL,
+    Prenom VARCHAR2(15) NOT NULL,
     -- Contraints
     CONSTRAINT pk_Proprio PRIMARY KEY (IdP),
     CONSTRAINT uk_Proprio UNIQUE (Nom, Prenom)
 );
 
-CREATE TABLE ESPACE(
-    -- Attributes
-    IdE NUMBER(6,0),
-    Designation VARCHAR2(20),
-    Superficie NUMBER(10,2),
-    IdERattach NUMBER(6,0),
-    -- Contrains
-    CONSTRAINT pk_IdE PRIMARY KEY (IdE),
-    CONSTRAINT fk_Espace FOREIGN KEY (IdERattach) REFERENCES BIEN(IdEBien)
-);
-
 CREATE TABLE BIEN(
     -- attributes
     IdEBien NUMBER(6,0),
-    Categorie VARCHAR2(15),
-    Type VARCHAR2(15),
-    IdP NUMBER(6,0),
-    Ville VARCHAR2(30),
-    Prix NUMBER(9, 2),
+    Categorie VARCHAR2(15) NOT NULL,
+    Type VARCHAR2(15) NOT NULL,
+    IdP NUMBER(6,0) NOT NULL,
+    Ville VARCHAR2(30) NOT NULL,
+    Prix NUMBER(9, 2) NOT NULL,
     -- Contrains
     CONSTRAINT ch_Categorie CHECK (Categorie IN ('Appartement', 'Maison de ville', 'Villa')),
     CONSTRAINT ch_Type CHECK (Type IN ('Studio', 'T2', 'T3', 'T4', 'T5')),
     CONSTRAINT pk_Bien PRIMARY KEY (IdEBien),
-    CONSTRAINT fk_Proprio FOREIGN KEY (IdP) REFERENCES PROPRIO(IdP),
-    -- CONSTRAINT fk_Bien_Espace FOREIGN KEY (IdEBien) REFERENCES ESPACE(IdE)
+    CONSTRAINT fk_Proprio FOREIGN KEY (IdP) REFERENCES PROPRIO(IdP)
 );
+
+CREATE TABLE ESPACE (
+    IdE NUMBER(6, 0),
+    Designation VARCHAR2(20) NOT NULL,
+    Superficie NUMBER(6, 0) NOT NULL,
+    IdeRattach NUMBER(6, 0),
+    CONSTRAINT pk_ESPACE PRIMARY KEY (IdE)
+);
+
+ALTER TABLE ESPACE
+ADD CONSTRAINT fk_Espace_Bien FOREIGN KEY (IdeRattach) REFERENCES BIEN(IdEBien);
